149acc67911b1ee4e03f274e24380fa4097ac256
Oleg <example@gmail.com>
2020-04-05 18:36:43 +0800

    added smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__back').eq(i).toggleClass('catalog-item__back_active');
             });
         });
     }
@@ -91,5 +92,40 @@ $(document).ready(function(){
     // Mask phone 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    // Mail
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+
+    // Smooth scroll and page up
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        }else{
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
 
